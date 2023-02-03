229806900102312ac9a6e1944df334b486e168fa
mai <fujisawamai21@gmail.com>
2023-02-03 14:31:08 +0900

    プロジェクトにeslintとprettierを導入した。

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      hoge
-    </div>
-  );
+  return <div className="App">hoge</div>;
 }
 
 export default App;
