b2e8a41e510603851d4ce5be94e8053aa2085a13
mai <fujisawamai21@gmail.com>
2023-02-27 23:33:56 +0900

    #3 配偶者特別控除の計算とテストの実装

--- src/models/calculatorModel/incomeDeductionCalculator.test.ts
+++ src/models/calculatorModel/incomeDeductionCalculator.test.ts
@@ -28,4 +28,64 @@ describe("IncomeDeductionCalculatorModel", () => {
     const result6 = calculator.calcSpouseDeduction(10000000, 480000);
     expect(result6).toBe(110000);
   });
+
+  test("配偶者特別控除の計算", () => {
+    const result1 = calculator.calcSpouseSpecialDeduction(500000, 5000000);
+    expect(result1).toBe(0);
+    const result2 = calculator.calcSpouseSpecialDeduction(10000001, 500000);
+    expect(result2).toBe(0);
+    const result3 = calculator.calcSpouseSpecialDeduction(5000000, 480000);
+    expect(result3).toBe(0);
+    const result4 = calculator.calcSpouseSpecialDeduction(5000000, 1330001);
+    expect(result4).toBe(0);
+    // 本人の合計所得金額900万円以下
+    const result5 = calculator.calcSpouseSpecialDeduction(9000000, 1000000);
+    expect(result5).toBe(330000);
+    const result6 = calculator.calcSpouseSpecialDeduction(9000000, 1050000);
+    expect(result6).toBe(310000);
+    const result7 = calculator.calcSpouseSpecialDeduction(9000000, 1100000);
+    expect(result7).toBe(260000);
+    const result8 = calculator.calcSpouseSpecialDeduction(9000000, 1150000);
+    expect(result8).toBe(210000);
+    const result9 = calculator.calcSpouseSpecialDeduction(9000000, 1200000);
+    expect(result9).toBe(160000);
+    const result10 = calculator.calcSpouseSpecialDeduction(9000000, 1250000);
+    expect(result10).toBe(110000);
+    const result11 = calculator.calcSpouseSpecialDeduction(9000000, 1300000);
+    expect(result11).toBe(60000);
+    const result12 = calculator.calcSpouseSpecialDeduction(9000000, 1330000);
+    expect(result12).toBe(30000);
+    // 本人の合計所得金額950万円以下
+    const result13 = calculator.calcSpouseSpecialDeduction(9500000, 1000000);
+    expect(result13).toBe(220000);
+    const result14 = calculator.calcSpouseSpecialDeduction(9500000, 1050000);
+    expect(result14).toBe(210000);
+    const result15 = calculator.calcSpouseSpecialDeduction(9500000, 1100000);
+    expect(result15).toBe(180000);
+    const result16 = calculator.calcSpouseSpecialDeduction(9500000, 1150000);
+    expect(result16).toBe(140000);
+    const result17 = calculator.calcSpouseSpecialDeduction(9500000, 1200000);
+    expect(result17).toBe(110000);
+    const result18 = calculator.calcSpouseSpecialDeduction(9500000, 1250000);
+    expect(result18).toBe(80000);
+    const result19 = calculator.calcSpouseSpecialDeduction(9500000, 1300000);
+    expect(result19).toBe(40000);
+    const result20 = calculator.calcSpouseSpecialDeduction(9500000, 1330000);
+    expect(result20).toBe(20000);
+    // 本人の合計所得金額1000万円以下
+    const result22 = calculator.calcSpouseSpecialDeduction(10000000, 1050000);
+    expect(result22).toBe(110000);
+    const result23 = calculator.calcSpouseSpecialDeduction(10000000, 1100000);
+    expect(result23).toBe(90000);
+    const result24 = calculator.calcSpouseSpecialDeduction(10000000, 1150000);
+    expect(result24).toBe(70000);
+    const result25 = calculator.calcSpouseSpecialDeduction(10000000, 1200000);
+    expect(result25).toBe(60000);
+    const result26 = calculator.calcSpouseSpecialDeduction(10000000, 1250000);
+    expect(result26).toBe(40000);
+    const result27 = calculator.calcSpouseSpecialDeduction(10000000, 1300000);
+    expect(result27).toBe(20000);
+    const result28 = calculator.calcSpouseSpecialDeduction(10000000, 1330000);
+    expect(result28).toBe(10000);
+  });
 });
--- src/models/calculatorModel/incomeDeductionCalculator.ts
+++ src/models/calculatorModel/incomeDeductionCalculator.ts
@@ -18,9 +18,49 @@ export class IncomeDeductionCalculatorModel {
     return 110000;
   }
 
+  // 配偶者特別控除
+  calcSpouseSpecialDeduction(myIncome: number, spouseIncome: number) {
+    if (myIncome < spouseIncome) return 0;
+    if (spouseIncome <= 480000 || spouseIncome > 1330000) return 0;
+    if (myIncome <= 9000000) {
+      if (spouseIncome <= 1000000) return 330000;
+      if (spouseIncome <= 1050000) return 310000;
+      if (spouseIncome <= 1100000) return 260000;
+      if (spouseIncome <= 1150000) return 210000;
+      if (spouseIncome <= 1200000) return 160000;
+      if (spouseIncome <= 1250000) return 110000;
+      if (spouseIncome <= 1300000) return 60000;
+      if (spouseIncome <= 1330000) return 30000;
+    }
+    if (myIncome <= 9500000) {
+      if (spouseIncome <= 1000000) return 220000;
+      if (spouseIncome <= 1050000) return 210000;
+      if (spouseIncome <= 1100000) return 180000;
+      if (spouseIncome <= 1150000) return 140000;
+      if (spouseIncome <= 1200000) return 110000;
+      if (spouseIncome <= 1250000) return 80000;
+      if (spouseIncome <= 1300000) return 40000;
+      if (spouseIncome <= 1330000) return 20000;
+    }
+    if (myIncome <= 10000000) {
+      if (spouseIncome <= 1050000) return 110000;
+      if (spouseIncome <= 1100000) return 90000;
+      if (spouseIncome <= 1150000) return 70000;
+      if (spouseIncome <= 1200000) return 60000;
+      if (spouseIncome <= 1250000) return 40000;
+      if (spouseIncome <= 1300000) return 20000;
+      if (spouseIncome <= 1330000) return 10000;
+    }
+    return 0;
+  }
+
   calcTotalIncomeDeduction(myIncome: number, spouseIncome: number): number {
     const basicDeduction = this.calcBasicDeduction(myIncome);
     const spouseDeduction = this.calcSpouseDeduction(myIncome, spouseIncome);
-    return basicDeduction + spouseDeduction;
+    const specialSpouseDeduction = this.calcSpouseSpecialDeduction(
+      myIncome,
+      spouseIncome
+    );
+    return basicDeduction + spouseDeduction + specialSpouseDeduction;
   }
 }
